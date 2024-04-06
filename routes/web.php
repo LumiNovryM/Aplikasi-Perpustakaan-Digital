7c210f72ab5fb958cf28a7f30ce7ff976221edde
LumiNovryM <luminovrymekel@gmail.com>
2024-04-07 00:51:15 +0700

    feat: create dashboard page and controller for all user role

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,29 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PeminjamController;
+use App\Http\Controllers\PetugasController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
+# Return Redirect To Login Page
+Route::get('/', function() {
+    return redirect('/login');
+});
+
+# Administrator Handler
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard_admin'])->name('dashboard_admin');
+});
+
+# Petugas Handler
+Route::prefix('petugas')->group(function () {
+    Route::get('/dashboard', [PetugasController::class, 'dasboard_petugas'])->name('dashboard_petugas');
+});
+
+# Peminjam Handler
+Route::prefix('peminjam')->group(function () {
+    Route::get('/dasboard', [PeminjamController::class, 'dashboard_peminjam'])->name('dashboard_peminjam');
 });
 
 Route::get('/login', [AuthController::class, 'login_page']);
