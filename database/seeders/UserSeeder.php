3cc8a82331c84aea68b3842c072f215c14fba796
LumiNovryM <luminovrymekel@gmail.com>
2024-04-07 01:05:59 +0700

    feat: create user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,11 +16,13 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['name' => 'Lumi Novry M', 'email' => 'lumi@gmail.com', 'email_verified_at' => null, 'role' => 'administrator' ,'password' => Hash::make('Mysql@2023'), 'remember_token' => null, 'created_at' => date(now()), 'updated_at' => null]
+            ['name' => 'Administrator', 'email' => 'administrator@gmail.com', 'email_verified_at' => null, 'role' => 'administrator', 'password' => Hash::make('Mysql@2023'), 'remember_token' => null, 'created_at' => date(now()), 'updated_at' => null],
+            ['name' => 'Petugas', 'email' => 'petugas@gmail.com', 'email_verified_at' => null, 'role' => 'petugas', 'password' => Hash::make('Mysql@2023'), 'remember_token' => null, 'created_at' => date(now()), 'updated_at' => null],
+            ['name' => 'Peminjam', 'email' => 'peminjam@gmail.com', 'email_verified_at' => null, 'role' => 'peminjam', 'password' => Hash::make('Mysql@2023'), 'remember_token' => null, 'created_at' => date(now()), 'updated_at' => null],
         ];
 
         foreach ($data as $val) {
-            User::insert(['name' => $val['name'], 'email' => $val['email'], 'email_verified_at' => $val['email_verified_at'], 'role' => $val['role'] ,'password' => $val['password'], 'remember_token' => $val['remember_token'], 'created_at' => $val['created_at'], 'updated_at' => $val['updated_at']]);
+            User::insert(['name' => $val['name'], 'email' => $val['email'], 'email_verified_at' => $val['email_verified_at'], 'role' => $val['role'], 'password' => $val['password'], 'remember_token' => $val['remember_token'], 'created_at' => $val['created_at'], 'updated_at' => $val['updated_at']]);
         }
     }
 }
